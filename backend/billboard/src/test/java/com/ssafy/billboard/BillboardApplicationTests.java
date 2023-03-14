6056917200c28deeebcaaeef9a5a175b91eb0932
jbh <0901jbh@naver.com>
2023-03-14 15:45:13 +0900

    jbh::feat:: make structure

---
+++ backend/billboard/src/test/java/com/ssafy/billboard/BillboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.billboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
