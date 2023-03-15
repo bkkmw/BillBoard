6808d48d23bcdc6087fd57967183184f6fe87b4f
HWANGSAN <gmwing0217@gmial.com>
2023-03-15 22:10:44 +0900

    hsnr

--- frontend/billboard/src/pages/profile/Profile.jsx
+++ frontend/billboard/src/pages/profile/Profile.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Main from '../main/Main';
+import { selectUser } from '../../store/user';
+import { useSelector } from 'react-redux';
 const Profile = () => {
+  const userId = useSelector(selectUser).loginUser.userId
   return (
     <div>
       Projile.jsx에서 작성한 내용
       <Link to='/'>메인페이지로</Link>
+      <div>유저이름:{userId}</div>
     </div>
   );
 };
--- frontend/billboard/src/store/store.js
+++ frontend/billboard/src/store/store.js
@@ -1,8 +1,34 @@
 import { combineReducers } from "redux";
 import user from "./user";
+import storage from "redux-persist/lib/storage";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import persistStore from "redux-persist/es/persistStore";
+import logger from 'redux-logger'
+
+// Todo: redux-persist에 유지할 내용 수정 예정
+const persistConfig = {
+  key: "root",
+  storage,
+    // Todo: whitelist 수정
+    // whitelist: ["user"] : user storage만 유지
+  whitelist: [],
+};
 
 const rootReducer = combineReducers({
   user,
 });
 
-export default rootReducer;
+const PersistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+const store = configureStore({
+  reducer: PersistedReducer,
+  // devTools:
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+const persistor = persistStore(store);
+const action = (type, payload) => store.dispatch({ type, payload })
+
+export {store, persistor, action}
