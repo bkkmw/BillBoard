ce620de05170fe9179488646d7dc9b349e13186e
황산나래 <cloudclick0217@gmail.com>
2023-03-17 11:39:20 +0900

    FEAT: 지도

---
+++ frontend/billboard/src/components/profile/ProfileExp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfileExp = () => {
+  return <div>경험치바</div>;
+};
+
+export default ProfileExp;
---
+++ frontend/billboard/src/components/profile/ProfileRating.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfileRating = () => {
+  return <div>전적</div>;
+};
+
+export default ProfileRating;
