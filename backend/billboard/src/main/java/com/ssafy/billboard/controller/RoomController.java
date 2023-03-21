ed88ccb5c3ccdbbf935746f5bb14c4541a0ecc8b
9jinseok <mer0120@naver.com>
2023-03-21 13:37:18 +0900

    feat: 방 생성, 조회, 삭제 API 구현

--- backend/billboard/src/main/java/com/ssafy/billboard/controller/RoomController.java
+++ backend/billboard/src/main/java/com/ssafy/billboard/controller/RoomController.java
@@ -3,25 +3,49 @@ package com.ssafy.billboard.controller;
 import com.ssafy.billboard.model.dto.RoomDto;
 import com.ssafy.billboard.model.entity.Room;
 import com.ssafy.billboard.model.repository.RoomRepository;
+import com.ssafy.billboard.model.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/room")
 public class RoomController {
 
     @Autowired
-    private RoomRepository roomRepository;
+    private RoomService roomService;
 
     @PostMapping()
-    public ResponseEntity<?> createRoom(@RequestBody RoomDto roomDto){
-        Room room = new Room.RoomBuilder(roomDto).build();
-        roomRepository.save(room);
-        return new ResponseEntity<>(room, HttpStatus.CREATED);
+    public ResponseEntity<?> createRoom(@RequestBody RoomDto.RoomInput roomInput){
+        roomService.createRoom(roomInput);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping()
+    public ResponseEntity<?> getRooms(){
+        List<Room> rooms = roomService.getRooms();
+        if(rooms.size() == 0)
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        else
+            return new ResponseEntity<>(rooms, HttpStatus.OK);
+    }
+
+    @GetMapping("/{roomId}")
+    public ResponseEntity<?> getRoom(@PathVariable int roomId){
+        Room room = roomService.getRoom(roomId);
+        if(room == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else
+            return new ResponseEntity<>(room, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{roomId}")
+    public ResponseEntity<?> deleteRoom(@PathVariable int roomId){
+        if(roomService.deleteRoom(roomId))
+            return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- backend/billboard/src/main/java/com/ssafy/billboard/model/entity/Reply.java
+++ backend/billboard/src/main/java/com/ssafy/billboard/model/entity/Reply.java
@@ -1,26 +1,29 @@
 package com.ssafy.billboard.model.entity;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name="reply")
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@DynamicInsert
+@Builder
 public class Reply {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
     private int replyId;
 
+    @Column(nullable = false)
     private int roomId;
 
+    @Column
     private String content;
 
+    @Column(nullable = false)
     private String userId;
 }
