07e3f24b657487ae605b4a9ddac70a8adaa63289
황산나래 <cloudclick0217@gmail.com>
2023-03-21 16:06:33 +0900

    FEAT: SVD

--- data/make_SVDpkl.py
+++ data/make_SVDpkl.py
@@ -1,19 +1,21 @@
+# SVD로 학습한 모델을 SVD.pkl로 저장
 import pandas as pd
 from surprise import Reader, Dataset
 from surprise.model_selection import train_test_split
-from surprise import SVD
+from surprise import SVD, SVDpp, SlopeOne, NMF, NormalPredictor, KNNBasic, KNNBaseline, KNNWithMeans, KNNWithZScore, BaselineOnly, CoClustering
 from surprise import accuracy
- 
+from surprise.model_selection import cross_validate
+import joblib
+
 ratings = pd.read_csv('./ratings.csv', encoding='UTF-8')
 reader = Reader(line_format='user item rating',sep=',',rating_scale=(1, 10))
- 
-# ratings DataFrame 에서 컬럼은 사용자 아이디, 아이템 아이디, 평점 순서를 지켜야 합니다.
+
 data = Dataset.load_from_df(ratings, reader)
 
 trainset, testset = train_test_split(data, test_size=.25, random_state=0)
-print(1)
- 
+
 algo = SVD(n_factors=10, random_state=0)
+
 algo.fit(trainset)
-predictions = algo.test( testset )
-accuracy.rmse(predictions)
+
+joblib.dump(algo, './SVD.pkl')
