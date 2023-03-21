e02032e50cf72e4fa43b30b9ba99ba88fb48ec2d
9jinseok <mer0120@naver.com>
2023-03-21 17:14:22 +0900

    feat: db 테이블 생성 쿼리 추가

---
+++ backend/billboard/src/main/resources/SQL/billboard.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS `follow`;
+DROP TABLE IF EXISTS `entry`;
+DROP TABLE IF EXISTS `reply`;
+DROP TABLE IF EXISTS `room`;
+DROP TABLE IF EXISTS `user`;
+
+-- 유저테이블 만들 자리 
+
+CREATE TABLE `room` (
+	`roomId` bigint NOT NULL AUTO_INCREMENT,
+	`hostId` varchar(45) NOT NULL,
+	`title` varchar(45) NOT NULL,
+	`personCount` int DEFAULT 0,
+	`personLimit` int NOT NULL,
+	`location` varchar(45) DEFAULT NULL,
+	`date` datetime DEFAULT NULL,
+	PRIMARY KEY (`roomId`),
+	FOREIGN KEY (hostId) REFERENCES user (userId) on delete cascade
+);
+
+CREATE TABLE `reply` (
+	`replyId` bigint NOT NULL AUTO_INCREMENT,
+	`roomId` bigint NOT NULL,
+	`content` varchar(60) NOT NULL,
+	`userId` varchar(45) NOT NULL,
+	PRIMARY KEY (`replyId`),
+	FOREIGN KEY (roomId) REFERENCES room (roomId) on delete cascade,
+	FOREIGN KEY (userId) REFERENCES user (userId) on delete cascade
+);
+
+CREATE TABLE `entry` (
+	`entryId` bigint NOT NULL AUTO_INCREMENT,
+	`roomId` bigint NOT NULL,
+	`userId` varchar(45) NOT NULL,
+	PRIMARY KEY (`entryId`),
+	FOREIGN KEY (roomId) REFERENCES room (roomId) on delete cascade,
+	FOREIGN KEY (userId) REFERENCES user (userId) on delete cascade
+);
+
+CREATE TABLE `follow` (
+	`followId` bigint NOT NULL AUTO_INCREMENT,
+	`fromUserId` varchar(45) NOT NULL,
+	`toUserId` varchar(45) NOT NULL,
+	PRIMARY KEY (`followId`),
+	FOREIGN KEY (fromUserId) REFERENCES user (userId) on delete cascade,
+	FOREIGN KEY (toUserId) REFERENCES user (userId) on delete cascade
+);
