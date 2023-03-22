4c111a419f730467c0232471768ce531d5e04500
bkkmw <roboticts49@gmail.com>
2023-03-22 13:27:50 +0900

    fix: builder pattern for DTO & ENTITY

--- backend/billboard/src/main/java/com/ssafy/billboard/model/dto/UserDto.java
+++ backend/billboard/src/main/java/com/ssafy/billboard/model/dto/UserDto.java
@@ -1,19 +1,43 @@
 package com.ssafy.billboard.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
-@Data
-@Builder
 public class UserDto {
 
-    private String userId;
-    private String password;
-    private String nickname;
-    private String email;
-    private String state;
-    private int matchCount;
-    private int winCount;
-    private int experience;
-    private String refreshToken;
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class UserInfoDto {
+
+        private String userId;
+        private String nickname;
+        private String email;
+        private int experience;
+        private int matchCount;
+        private int winCount;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class UserLoginDto {
+        private String userId;
+        private String password;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class UserSignUpDto {
+
+        private String userId;
+        private String password;
+        private String nickname;
+        private String email;
+    }
+
+
 }
