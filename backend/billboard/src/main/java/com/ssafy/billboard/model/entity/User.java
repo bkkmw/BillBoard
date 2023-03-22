8ab0067f05884ce785da7231c8e32da0b7d168ba
bkkmw <roboticts49@gmail.com>
2023-03-22 14:02:33 +0900

    fix: BE-Convention

--- backend/billboard/src/main/java/com/ssafy/billboard/model/entity/User.java
+++ backend/billboard/src/main/java/com/ssafy/billboard/model/entity/User.java
@@ -1,6 +1,5 @@
 package com.ssafy.billboard.model.entity;
 
-import com.ssafy.billboard.model.dto.UserSignUpDto;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
