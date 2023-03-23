b8d6446bb5fed49879a8207fc123dbd7953d5343
임길현 <limgilhyun@naver.com>
2023-03-23 09:04:43 +0900

    Fix:navbar

--- frontend/billboard/src/components/mainPage/MainPage.jsx
+++ frontend/billboard/src/components/mainPage/MainPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import style from "./MainPage.module.css";
+import UserRecommendation from "./UserRecommendation";
 
 const Main = () => {
-  return <div>황산나래, 지혁주 Main.jsx에서 작성된 내용</div>;
+  return (
+    <div className={style.background}>
+      황산나래, 지혁주 Main.jsx에서 작성된 내용
+      <UserRecommendation />
+    </div>
+  );
 };
 
 export default Main;
