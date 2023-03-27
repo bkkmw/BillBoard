810ce54f95df43484bb6a2b58a0198e5af1763c8
임길현 <limgilhyun@naver.com>
2023-03-27 17:55:13 +0900

    Feat:메인페이지 레이아웃

---
+++ frontend/billboard/src/components/mainPage/Review.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Carousel from "./Carousel";
+
+const Review = () => {
+  return (
+    <div>
+      <span>리뷰 많은순</span>
+      <Carousel />
+    </div>
+  );
+};
+
+export default Review;
