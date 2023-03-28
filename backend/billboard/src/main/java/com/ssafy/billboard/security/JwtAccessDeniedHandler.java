1074a58c4115cdcf40af2bf5dc415e63ae0a2907
bkkmw <roboticts49@gmail.com>
2023-03-28 14:00:31 +0900

    feat: jwt token filter chain

---
+++ backend/billboard/src/main/java/com/ssafy/billboard/security/JwtAccessDeniedHandler.java
@@ -0,0 +1,25 @@
+package com.ssafy.billboard.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(JwtAccessDeniedHandler.class);
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        logger.info("ACCESS DENIED EXCEPTION");
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
