9ce8bc233965435baf0c37270eb9c4d5a67ea9e8
임길현 <limgilhyun@naver.com>
2023-03-29 11:24:14 +0900

    Style:프로필페이지, 매칭 조절

--- frontend/billboard/src/components/reserve/ReserveFind.jsx
+++ frontend/billboard/src/components/reserve/ReserveFind.jsx
@@ -1,114 +1,157 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
-import { Button, Col, List, Row } from 'antd';
-import ReserveFindAddress from './ReserveFindAddress';
-import { getRoom } from '../../store/reserve';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Button, Col, List, Row } from "antd";
+import ReserveFindAddress from "./ReserveFindAddress";
+import { getRoom } from "../../store/reserve";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { display } from "@mui/system";
 
-let dateList = []
+let dateList = [];
 for (let i = 0; i < 7; i++) {
-  const today = new Date()
-  today.setDate(today.getDate() + i)
-  dateList.push(today.toLocaleDateString())
+  const today = new Date();
+  today.setDate(today.getDate() + i);
+  dateList.push(today.toLocaleDateString());
 }
 
 const ReserveFind = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const [address, setAddress] = useState('')
-  const [isAddressOpen, setIsAddressOpen] = useState(false)
-  const [isChildAddressOpen, setIsChildAddressOpen] = useState(false)
-  const [date, setDate] = useState(new Date())
-  const [rooms, setRooms] = useState()
-  const [filterRooms, setFilterRooms] = useState()
-  const [coordinate, setCoordinate] = useState()
-  const [sortedRooms, setSortedRooms] = useState()
-  // Todo: rejected 
+  const [address, setAddress] = useState("");
+  const [isAddressOpen, setIsAddressOpen] = useState(false);
+  const [isChildAddressOpen, setIsChildAddressOpen] = useState(false);
+  const [date, setDate] = useState(new Date());
+  const [rooms, setRooms] = useState();
+  const [filterRooms, setFilterRooms] = useState();
+  const [coordinate, setCoordinate] = useState();
+  const [sortedRooms, setSortedRooms] = useState();
+  // Todo: rejected
   useEffect(() => {
     dispatch(getRoom())
       .then((data) => {
-
-        setRooms(rooms => data.payload.rooms)
-        getFilter()
+        setRooms((rooms) => data.payload.rooms);
+        getFilter();
       })
-      .catch((error) => { console.log(error) })
-  }, [])
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   // 날짜, 방정보가 갱신될때마다 보일 방 목록 필터링
   useEffect(() => {
-    getFilter()
-  }, [date, rooms])
+    getFilter();
+  }, [date, rooms]);
   // 방목록이 필터링된 후, 지역정보가 바뀔때마다 거리순으로 정렬렬
   useEffect(() => {
-    sortRoom()
-  }, [filterRooms, coordinate])
+    sortRoom();
+  }, [filterRooms, coordinate]);
   const sortRoom = () => {
     if (coordinate && filterRooms) {
-
       const newFilter = filterRooms.sort(function (a, b) {
-        const xy_a = a.location.split('$')
-        const xy_b = b.location.split('$')
+        const xy_a = a.location.split("$");
+        const xy_b = b.location.split("$");
 
         // console.log(getDistance(xy_a), getDistance(xy_b))
         if (getDistance(xy_a) > getDistance(xy_b)) {
-          return 1
+          return 1;
+        } else {
+          return -1;
         }
-        else { return -1 }
+      });
 
-      })
-
-      setSortedRooms([...newFilter])
+      setSortedRooms([...newFilter]);
+    } else {
+      setSortedRooms(filterRooms);
     }
-    else {
-      setSortedRooms(filterRooms)
-    }
-  }
+  };
   async function getFilter() {
     if (rooms) {
-      let filter = []
+      let filter = [];
       for (const room of rooms) {
-
-        if (new Date(room.date).toLocaleDateString() === date.toLocaleDateString()) {
-
-          filter.push(room)
-
+        if (
+          new Date(room.date).toLocaleDateString() === date.toLocaleDateString()
+        ) {
+          filter.push(room);
         }
       }
-      setFilterRooms(filterRooms => filter)
+      setFilterRooms((filterRooms) => filter);
     }
   }
   const getDistance = (arr) => {
-    return (arr[0] - coordinate.x) ** 2 + (arr[1] - coordinate.y) ** 2
-  }
+    return (arr[0] - coordinate.x) ** 2 + (arr[1] - coordinate.y) ** 2;
+  };
 
   return (
-    <div>
+    <div style={{ width: "70vw", marginTop: "10vh" }}>
       <Row>
         <Col span={12}>
           <Row>
-            <Button type='primary' onClick={() => { setIsAddressOpen(true) }}>{address === '' ? '지역별' : address}</Button>
+            <Button
+              type="primary"
+              onClick={() => {
+                setIsAddressOpen(true);
+              }}
+              style={{
+                width: "10vw",
+                height: "7vh",
+                borderRadius: "2rem",
+                marginBottom: "1rem",
+                fontSize: "2rem",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {address === "" ? "지역별" : address}
+            </Button>
           </Row>
           <Row>
             <List
+              style={{
+                borderTop: "1.5rem solid #d9d9d9",
+                borderBottom: "2rem solid #d9d9d9",
+                borderLeft: "1rem solid #d9d9d9",
+                borderRight: "1rem solid #d9d9d9",
+                borderRadius: "1.5rem",
+              }}
               dataSource={dateList}
               renderItem={(item) => (
-                <List.Item className={`list_item_hover ${item === date.toLocaleDateString() && 'list_item_select'}`} onClick={() => {
-                  setDate(new Date(item))
-                }}>
+                <List.Item
+                  style={{
+                    fontSize: "2.5rem",
+                    paddingRight: "2rem",
+                    paddingLeft: "2rem",
+                  }}
+                  className={`list_item_hover ${
+                    item === date.toLocaleDateString() && "list_item_select"
+                  }`}
+                  onClick={() => {
+                    setDate(new Date(item));
+                  }}
+                >
                   {item}
                 </List.Item>
               )}
             />
           </Row>
-          <ReserveFindAddress Open={isAddressOpen} childrenDrawer={isChildAddressOpen} setAddress={setAddress} setChildrenDrawer={setIsChildAddressOpen} onClose={() => setIsAddressOpen(false)} setCoordinate={setCoordinate} />
+          <ReserveFindAddress
+            Open={isAddressOpen}
+            childrenDrawer={isChildAddressOpen}
+            setAddress={setAddress}
+            setChildrenDrawer={setIsChildAddressOpen}
+            onClose={() => setIsAddressOpen(false)}
+            setCoordinate={setCoordinate}
+          />
         </Col>
         <Col span={12}>
           <List
             dataSource={sortedRooms}
             renderItem={(item) => (
-              <List.Item className={`list_item_hover ${item === date.toLocaleDateString() && 'list_item_select'}`}>
-                <Link to={`/room/${item.roomId}`}>{
-                  `날짜:${item.date},
+              <List.Item
+                className={`list_item_hover ${
+                  item === date.toLocaleDateString() && "list_item_select"
+                }`}
+              >
+                <Link to={`/room/${item.roomId}`}>{`날짜:${item.date},
       방장ID:${item.hostID},
       location:${item.location},
       personCount:${item.personCount},
