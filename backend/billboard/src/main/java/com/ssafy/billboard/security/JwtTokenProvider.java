21a25dbc59ccde0ab2e9fc1eff0fb5eb22d17cea
bkkmw <roboticts49@gmail.com>
2023-03-29 13:07:27 +0900

    feat: check refresh token and generate accesstoken

--- backend/billboard/src/main/java/com/ssafy/billboard/security/JwtTokenProvider.java
+++ backend/billboard/src/main/java/com/ssafy/billboard/security/JwtTokenProvider.java
@@ -21,8 +21,8 @@ public class JwtTokenProvider {
     private final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
     private final SecretKey SECRET_KEY;
 
-    private final long ACCESS_TOKEN_EXPIRES_AT = 5 * 60 * 1000L;
-    private final long REFRESH_TOKEN_EXPIRES_AT = 1 * 60 * 60 * 1000L;
+    private final long ACCESS_TOKEN_EXPIRES_AT = 1 * 60 * 1000L;
+    private final long REFRESH_TOKEN_EXPIRES_AT = 1 * 3 * 60 * 1000L;
 
     public JwtTokenProvider(@Value("${jwt.secret-key}")String key) {
         byte[] keyBytes = Decoders.BASE64.decode(key);
@@ -30,37 +30,63 @@ public class JwtTokenProvider {
     }
 
     public String[] generateToken(String userId) {
-        logger.info("create token : {}", userId);
-        Claims claims = Jwts.claims();
-        claims.setSubject("token");
-        claims.put("name", userId);
+        logger.info("create both token : {}", userId);
 
         Long currentTime = System.currentTimeMillis();
-        claims.setIssuedAt(new Date(currentTime));
-        logger.info("current time : {}", new Date(currentTime).toString());
 
-        String accessToken = Jwts.builder()
-                .setClaims(claims)
-                .setExpiration(new Date(currentTime + ACCESS_TOKEN_EXPIRES_AT))
-                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
-                .compact();
-
-        String refreshToken = Jwts.builder()
-                .setClaims(claims)
-                .setExpiration(new Date(currentTime + REFRESH_TOKEN_EXPIRES_AT))
-                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
-                .compact();
+        String accessToken = generateAccessToken(userId, currentTime);
+        String refreshToken = generateRefreshToken(userId, currentTime);
 
         logger.info("created token : {}, {}", accessToken, refreshToken);
         return new String[] {accessToken, refreshToken};
     }
 
+    public String generateAccessToken(String userId, long iat) {
+        logger.trace("create access token : {}", userId);
+        Claims claims = Jwts.claims();
+        claims.setSubject("access");
+        claims.put("name", userId);
+
+        claims.setIssuedAt(new Date(iat));
+        logger.info("current time : {}", new Date(iat).toString());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setExpiration(new Date(iat + ACCESS_TOKEN_EXPIRES_AT))
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateRefreshToken(String userId, long iat) {
+        logger.trace("create refresh token : {}", userId);
+        Claims claims = Jwts.claims();
+        claims.setSubject("refresh");
+        claims.put("name", userId);
+
+        claims.setIssuedAt(new Date(iat));
+        logger.info("current time : {}", new Date(iat).toString());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setExpiration(new Date(iat + REFRESH_TOKEN_EXPIRES_AT))
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    /**
+     * validate access token
+     * @param accessToken
+     * @return +1 for expired token, 0 for valid token, -1 for invalid token
+     */
     public int validateAccessToken(String accessToken) {
         try {
-            Jwts.parserBuilder()
+            Claims claims = Jwts.parserBuilder()
                     .setSigningKey(SECRET_KEY)
                     .build()
-                    .parseClaimsJws(accessToken);
+                    .parseClaimsJws(accessToken)
+                    .getBody();
+
+            if(!"access".equals(claims.getSubject())) return -1;
 
             logger.info("VALID TOKEN");
             return 0;
@@ -70,10 +96,42 @@ public class JwtTokenProvider {
             return -1;
         } catch (ExpiredJwtException e) {
             logger.info("Token has been expired");
+            logger.info("Expired token of : {}", e.getClaims().get("name"));
             return 1;
         } catch (SignatureException e) {
             logger.info("Failed to check signature");
             return -1;
+        }
+    }
+
+    /**
+     * validate refresh token
+     * @param refreshToken
+     * @return userId if valid, 'EXPIRED :userId' if expired, null if invalid
+     */
+    public String validateRefreshToken(String refreshToken) {
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(SECRET_KEY)
+                    .build()
+                    .parseClaimsJws(refreshToken)
+                    .getBody();
+
+            if(!"refresh".equals(claims.getSubject())) return null;
+
+            logger.info("VALID TOKEN");
+            return (claims.get("name") instanceof String) ? (String)claims.get("name") : null;
+        } catch (UnsupportedJwtException | MalformedJwtException | IllegalStateException e) {
+            logger.info("Invalid refresh token : {}", refreshToken);
+            logger.info("Exception : {}", e.getMessage());
+            return null;
+        } catch (ExpiredJwtException e) {
+            logger.info("Token has been expired");
+            logger.info("Expired token : {}", e.getClaims().get("name"));
+            return String.format("EXPIRED :%s", e.getClaims().get("name")).toString();
+        } catch (SignatureException e) {
+            logger.info("Failed to check signature");
+            return null;
         }
     }
 
