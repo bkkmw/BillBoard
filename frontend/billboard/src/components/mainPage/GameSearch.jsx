eb6d1d7fb4063a7771c8c051ad086cd0f81ed0c6
seotai78@naver.com <seotai78@naver.com>
2023-03-30 16:53:41 +0900

    feat:보드게임조회

--- frontend/billboard/src/components/mainPage/GameSearch.jsx
+++ frontend/billboard/src/components/mainPage/GameSearch.jsx
@@ -1,16 +1,47 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Grid from "@mui/material/Grid";
 
 export default function GameSearch() {
+  const checkboxes = [
+    { label: "커스텀 게임", checked: false },
+    { label: "가족 게임", checked: false },
+    { label: "파티 게임", checked: false },
+    { label: "추상 게임", checked: false },
+    { label: "어린이 게임", checked: false },
+    { label: "전쟁 게임", checked: false },
+    { label: "전략 게임", checked: false },
+    { label: "영화적 게임", checked: false },
+  ];
+
   return (
-    <Box
-      sx={{
-        width: "70vw",
-        maxWidth: "100%",
-      }}
-    >
-      <TextField fullWidth label="게임 이름을 입력하시오" id="gamesearch" />
-    </Box>
+    <>
+      {/* 게임 이름 */}
+      <Box
+        sx={{
+          width: "70vw",
+          maxWidth: "100%",
+        }}
+      >
+        <TextField fullWidth label="게임 이름을 입력하시오" id="gamesearch" />
+      </Box>
+      {/* 장르선택 */}
+      <FormGroup style={{ textAlign: "start" }}>
+        <Grid container spacing={3}>
+          {checkboxes.map((checkbox, index) => (
+            <Grid item xs={3} key={index}>
+              <FormControlLabel
+                control={<Checkbox defaultChecked={checkbox.checked} />}
+                label={checkbox.label}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </FormGroup>
+    </>
   );
 }
--- frontend/billboard/src/components/mainPage/MainPage.jsx
+++ frontend/billboard/src/components/mainPage/MainPage.jsx
@@ -1,30 +1,163 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import style from "./MainPage.module.css";
 import UserRecommend from "./UserRecommend";
 import GameSearch from "./GameSearch";
-import GanreSearch from "./GanreSearch";
-// import Rank from "./Rank";
-// import Grade from "./Grade";
-// import Review from "./Review";
-// import Recent from "./Recent";
-// import Level from "./Level";
-import Carousel from "./Carousel";
+import { useDispatch } from "react-redux";
+
+import { getBoardGames } from "../../store/boardgames";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+import "./MainPage.css";
+
+// import required modules
+import { FreeMode, Pagination, Navigation } from "swiper";
+
 const Main = () => {
+  const dispatch = useDispatch();
+  const [gameData, setGameData] = useState({
+    name: "",
+    maxplaytime: 1000,
+    maxplayers: 5,
+    average: 0,
+    averageWeight: 0,
+    strategy: "",
+    family: "",
+    party: "",
+    abstract: "",
+    thematic: "",
+    war: "",
+    customizable: "",
+    children: "",
+  });
+  // 평균, 랭킹, 난이도, 리뷰, 날짜순 기본 추천
+  const [boardAverage, setBoardAverage] = useState([]);
+  const [boardRanking, setBoardRanking] = useState([]);
+  const [boardWeight, setBoardWeight] = useState([]);
+  const [boardReview, setBoardReview] = useState([]);
+  const [boardDate, setBoardDate] = useState([]);
+
+  // 기본 추천 목록조회 api
+  useEffect(() => {
+    const boards = () => {
+      dispatch(getBoardGames(gameData)).then((response) => {
+        // console.log("ajdla", response.payload["average"]);
+        setBoardAverage(response.payload["average"]);
+        setBoardRanking(response.payload["rank"]);
+        setBoardWeight(response.payload["weights"]);
+        setBoardReview(response.payload["review"]);
+        setBoardDate(response.payload["yearpublished"]);
+      });
+    };
+    boards();
+  }, []);
+  // console.log(boardAverage);
   return (
     <div className={style.background}>
       <span>유저별</span>
       <UserRecommend />
       <GameSearch />
-      <GanreSearch />
+
+      <div>
+        <span>평균순</span>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[FreeMode, Pagination, Navigation]}
+          className="boardAverage"
+        >
+          {boardAverage.map((data, index) => (
+            <SwiperSlide key={index}>
+              <img src={data.image} alt="Slide 1" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
       <div>
         <span>랭킹순</span>
-        <Carousel />
-        <span>평점순</span>
-        <Carousel />
-        <span>최신순</span>
-        <Carousel />
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="boardRanking"
+        >
+          {boardRanking.map((data, index) => (
+            <SwiperSlide key={index}>
+              <img src={data.image} alt="Slide 1" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div>
         <span>난이도순</span>
-        <Carousel />
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="boardWeight"
+        >
+          {boardWeight.map((data, index) => (
+            <SwiperSlide key={index}>
+              <img src={data.image} alt="Slide 1" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div>
+          <span>리뷰순</span>
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            freeMode={true}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[FreeMode, Pagination]}
+            className="boardReview"
+          >
+            {boardReview.map((data, index) => (
+              <SwiperSlide key={index}>
+                <img src={data.image} alt="Slide 1" />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div>
+          <span>출시일순</span>
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            freeMode={true}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[FreeMode, Pagination]}
+            className="boardDate"
+          >
+            {boardDate.map((data, index) => (
+              <SwiperSlide key={index}>
+                <img src={data.image} alt="Slide 1" />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
     </div>
   );
