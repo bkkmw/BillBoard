06d954a0b6a499985ca074c7c8c8b3cff12597e5
seotai78@naver.com <seotai78@naver.com>
2023-03-31 14:56:39 +0900

    feat:즐겨찾기,상세페이지

--- frontend/billboard/src/components/detail/GameReview.jsx
+++ frontend/billboard/src/components/detail/GameReview.jsx
@@ -1,12 +1,89 @@
-import React from "react";
 import style from "./GameReview.module.css";
-import GamePagination from "./GamePagination";
 
-const GameReview = () => {
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+import { Table, Button } from "antd";
+import "./GamePagination.css";
+
+const GameReview = (props) => {
+  const [dataSource, setDataSource] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    fetchRecords(1);
+  }, []);
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "_id",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Trips",
+      dataIndex: "trips",
+    },
+    {
+      title: "Delete",
+      render: (index, record) => (
+        <Button onClick={() => handleDelete(record)}>Delete</Button>
+      ),
+    },
+  ];
+
+  const fetchRecords = (page) => {
+    setLoading(true);
+    axios
+      .get(`https://api.instantwebtools.net/v1/passenger?page=${page}&size=10`)
+      .then((res) => {
+        const dataWithKeys = res.data.data.map((row) => ({
+          ...row,
+          key: row._id, // Use the _id field as the unique key
+        }));
+        setDataSource(dataWithKeys);
+        setTotalPages(res.data.totalPages);
+        setLoading(false);
+      });
+  };
+
   return (
     <div>
       <span className={style.font}>Review</span>
-      <GamePagination />
+
+      <div
+        className="fontSize"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Table
+          className="my-table"
+          loading={loading}
+          columns={columns}
+          dataSource={dataSource}
+          style={{ width: "70vw" }}
+          pagination={{
+            pageSize: 10,
+            total: totalPages,
+            onChange: (page) => {
+              fetchRecords(page);
+            },
+            showSizeChanger: false,
+            style: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              fontSize: "1.5rem",
+            },
+          }}
+          rowKey={(record) => record.key}
+        ></Table>
+      </div>
     </div>
   );
 };
