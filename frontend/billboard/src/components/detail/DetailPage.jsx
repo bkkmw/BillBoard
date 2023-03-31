fc3c235c7c9cfadafafef659d16ce806551f44f2
황산나래 <cloudclick0217@gmail.com>
2023-03-31 12:36:54 +0900

    FIX/해결

--- frontend/billboard/src/components/detail/DetailPage.jsx
+++ frontend/billboard/src/components/detail/DetailPage.jsx
@@ -3,10 +3,13 @@ import GameDetail from "./GameDetail";
 import GameDescription from "./GameDescription";
 import GameVideo from "./GameVideo";
 import GameReview from "./GameReview";
+import { useRouteLoaderData } from "react-router";
 
 const Detail = () => {
+  const gameId = useRouteLoaderData("detail");
   return (
     <div>
+      
       <GameDetail />
       <hr style={{ width: "74vw" }} />
       <GameDescription />
