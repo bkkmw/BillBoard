66e98552906a34443e10896fe4e986b7ef955e18
임길현 <limgilhyun@naver.com>
2023-04-01 19:02:56 +0900

    Style:게임중, 디테일 페이지

--- frontend/billboard/src/components/detail/DetailPage.jsx
+++ frontend/billboard/src/components/detail/DetailPage.jsx
@@ -4,19 +4,20 @@ import GameDescription from "./GameDescription";
 import GameVideo from "./GameVideo";
 import GameReview from "./GameReview";
 import { useLocation } from "react-router";
+import { Margin } from "@mui/icons-material";
 
 const Detail = () => {
   const location = useLocation();
   // console.log(location);
   const details = location.state;
   return (
-    <div style={{ width: "100%", height: "50vh" }}>
+    <div style={{ width: "100%", height: "75vh" }}>
       <GameDetail details={details} />
-      <hr style={{ width: "74vw" }} />
+      <hr style={{ width: "74vw", marginBottom: "3vh" }} />
       <GameDescription details={details} />
-      <hr style={{ width: "74vw" }} />
+      <hr style={{ width: "74vw", margin: "3vh 0 3vh 0" }} />
       <GameVideo key="key" details={details} />
-      <hr style={{ width: "74vw" }} />
+      <hr style={{ width: "74vw", margin: "3vh 0 3vh 0" }} />
       <GameReview details={details} />
     </div>
   );
--- frontend/billboard/src/components/gameroom/RoomRight.jsx
+++ frontend/billboard/src/components/gameroom/RoomRight.jsx
@@ -102,6 +102,63 @@ const RoomRight = () => {
         게임 시작
       </Button>
     </div>
+
+    // * 밑에 있는게 게임중 보이는 화면 *
+
+    // <div
+    //   style={{
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     justifyContent: "space-evenly",
+    //     width: "43vw",
+    //     marginRight: "1.5rem",
+    //   }}
+    // >
+    //   <div
+    //     style={{
+    //       backgroundColor: "#f9f9f9",
+    //       height: "55vh",
+    //       display: "flex",
+    //       flexDirection: "column",
+    //       justifyContent: "space-evenly",
+    //     }}
+    //   >
+    //     <div
+    //       style={{
+    //         width: "43vw",
+    //         display: "flex",
+    //         flexDirection: "row",
+    //         justifyContent: "space-evenly",
+    //       }}
+    //     >
+    //       <span style={{ fontSize: "3vw" }}>16:40</span>
+    //       <span style={{ fontSize: "3vw" }}>타이머</span>
+    //     </div>
+    //     <div
+    //       style={{
+    //         display: "flex",
+    //         flexDirection: "column",
+    //       }}
+    //     >
+    //       <img
+    //         src="https://cataas.com/cat"
+    //         alt="보드게임"
+    //         style={{ width: "18vw", height: "30vh", marginLeft: "13vw" }}
+    //       />
+    //       <span style={{ fontSize: "2vw", marginTop: "3vh" }}>보드게임</span>
+    //     </div>
+    //   </div>
+    //   <Button
+    //     style={{
+    //       width: "10vw",
+    //       height: "5vh",
+    //       fontSize: "2rem",
+    //       marginLeft: "17vw",
+    //     }}
+    //   >
+    //     게임 종료
+    //   </Button>
+    // </div>
   );
 };
 
